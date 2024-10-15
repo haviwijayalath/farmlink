0f92ebcb0929930c9a1ea216027789204e8f3e03
pawaniKumari <140165230+pawaniKumari@users.noreply.github.com>
2024-10-15 16:44:43 +0530

    modify navbar

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -1,11 +1,11 @@
 <!-- navbar.php -->
 <header class="header">
     <div class="logo">
-        <img src="<?= URLROOT ?>/public/images/logo.png" alt="FarmLink Logo" style="width:200px;"> <!-- Replace with your logo -->
+        <img src="<?= URLROOT ?>/public/images/logo.jpeg" alt="FarmLink Logo" style="width:50px;"> <!-- Replace with your logo -->
     </div>
     <div style="display: flex; gap: 30px; font-size: 20px; font-family: 'Poppins', sans-serif;">
         <span><a href="home.php" ><i class="fa-solid fa-house"></i> Home</a></span> 
         <span><a href="aboutUs.php"><i class="fa-solid fa-info-circle"></i> About Us</a></span>
-        <span><a href="app/view/users/login.php" class="login-btn"><i class="fa-solid fa-user"></i> Login</a></span>
+        <span><a href="login.php" class="login-btn"><i class="fa-solid fa-user"></i> Login</a></span>
     </div>
 </header>
