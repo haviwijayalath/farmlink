8f06f844311ca6b9498f10f99299d5ae7af751b3
pawaniKumari <140165230+pawaniKumari@users.noreply.github.com>
2024-10-15 16:35:22 +0530

    include navbar and footer in header

---
+++ app/views/inc/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="<?= URLROOT ?>/public/css/navbar.css">
+  <link rel="stylesheet" href="<?= URLROOT ?>/public/css/footer.css">
+
+  
+  <!-- Add Google Fonts link -->
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500&display=swap" rel="stylesheet">
+  <!-- Add Font Awesome for icons -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+  <title><?php echo SITENAME; ?></title>
+</head>
+<body>
+<?php require APPROOT . '/view/inc/navbar.php'; ?>
+  
+
+
+
