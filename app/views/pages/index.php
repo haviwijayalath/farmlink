72793c695ae60d0f2be023fa96f98c8f405323aa
pawaniKumari <140165230+pawaniKumari@users.noreply.github.com>
2024-11-05 22:21:16 +0530

    modify footer, navbar,index

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,3 +1,6 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
+
+<?php require APPROOT . '/views/inc/sidebars/d_person.php'; ?>
+
 <h1> <?php echo $data['title']; ?></h1>
 <?php require APPROOT . '/views/inc/footer.php'; ?>
