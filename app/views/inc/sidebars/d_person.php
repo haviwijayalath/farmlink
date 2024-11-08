85100b32d4a0e9810421e96f8abf70c16d12b9f4
pawaniKumari <140165230+pawaniKumari@users.noreply.github.com>
2024-11-08 11:24:18 +0530

    creating d_person's sidebar

---
+++ app/views/inc/sidebars/d_person.php
@@ -0,0 +1,12 @@
+<!-- Sidebar -->
+<link rel="stylesheet" href="<?= URLROOT ?>/public/css/sidebars/d_person.css">
+
+<div class="sidebar">
+      <ul>
+        <li><a href="<?php echo URLROOT?>/orders/neworder"><i class="fas fa-shopping-cart"></i> New Orders</a></li>
+        <li><a href="<?php echo URLROOT?>/Dpersoncontrollers/viewprofile"><i class="fas fa-user"></i> Account</a></li>
+        <li><a href="<?php echo URLROOT?>/vehicles/vehicleinfo"><i class="fas fa-truck"></i> Vehicle</a></li>
+        <li><a href="<?php echo URLROOT?>/orders/orderhistory"><i class="fas fa-history"></i> Delivery History</a></li>
+        <li><a href="<?php echo URLROOT?>/orders/ongoing"><i class="fa-solid fa-truck-fast"></i>Ongoing</a></li>
+      </ul>
+    </div>
