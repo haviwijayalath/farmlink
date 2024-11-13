c8ab1308325908dbefab1ce0e02982dca25a5f9a
pawaniKumari <140165230+pawaniKumari@users.noreply.github.com>
2024-11-13 12:31:16 +0530

    modify the account models to retrieve informations of address

--- app/models/Account.php
+++ app/models/Account.php
@@ -7,25 +7,49 @@ class Account extends Database {
     }
 
     public function getUserById($id) {
-        $this->db->query('SELECT * FROM delivery_persons WHERE id = :id');
-        $this->db->bind(':id', $id);
-        return $this->db->single();
-    }
+            // Update query to join with the addresses table
+            $this->db->query('
+                SELECT dp.id, dp.name, dp.email, dp.phone, dp.area, dp.image, dp.address_id, dp.password,
+                       a.number, a.street, a.city
+                FROM delivery_persons dp
+                LEFT JOIN address a ON dp.address_id = a.address_id
+                WHERE dp.id = :id
+            ');
+    
+            $this->db->bind(':id', $id);
+            return $this->db->single();
+        }
 
     public function updateUser($data) {
-        $this->db->query('UPDATE delivery_persons SET name = :name, email = :email, phone = :phone, address = :address, area = :area, password = :password, image = :image WHERE id = :id');
+
+         // Update address details in the address table
+         $this->db->query('
+         UPDATE address 
+         SET number = :addr_no, street = :street, city = :city
+            WHERE address_id = :address_id
+        ');
+        $this->db->bind(':addr_no', $data['addr_no']);
+        $this->db->bind(':street', $data['street']);
+        $this->db->bind(':city', $data['city']);
+        $this->db->bind(':address_id', $data['address_id']);
+        $addressUpdated = $this->db->execute();
+
+        $this->db->query('UPDATE delivery_persons SET name = :name, email = :email, phone = :phone, address_id = :address_id, 
+                                area = :area, password = :password, image = :image WHERE id = :id');
         
         // Bind values
         $this->db->bind(':id', $data['id']);
         $this->db->bind(':name', $data['name']);
         $this->db->bind(':email', $data['email']);
         $this->db->bind(':phone', $data['phone']);
-        $this->db->bind(':address', $data['address']);
+        $this->db->bind(':address_id', $data['address_id']);
         $this->db->bind(':area', $data['area']);
         $this->db->bind(':password', $data['password']);
         $this->db->bind(':image', $data['image']);
+        $userUpdated = $this->db->execute();
+        
+       
 
-        // Execute
-        return $this->db->execute();
+        return $userUpdated && $addressUpdated;
     }
 }
