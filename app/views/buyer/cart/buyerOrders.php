c8876714628accb1a1c467389aaa432eb9b6c3a5
Hasitha Dilshan <kah.dilshan@gmail.com>
2024-11-26 16:04:16 +0530

    create order history page

--- app/views/buyer/cart/buyerOrders.php
+++ app/views/buyer/cart/buyerOrders.php
@@ -4,7 +4,38 @@
 
 <?php require APPROOT . '/views/inc/sidebars/buyer_sidebar.php'; ?>
 
-
+<div class="history-container">
+    <div class="history-content-area">
+        <h2>Your Orders</h2>
+        <table class="order-history-table">
+            <thead>
+                <tr>
+                    <th>Product</th>
+                    <th>Amount</th>
+                    <th>Shipping Address</th>
+                    <th>Order Date</th>
+                    <th>Status</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Carrot</td>
+                    <td>50 kg</td>
+                    <td>123-B, 2nd Lane, Embilipitiya</td>
+                    <td>2024-11-25</td>
+                    <td>Delivered</td>
+                </tr>
+                <tr>
+                    <td>Gova</td>
+                    <td>100 kg</td>
+                    <td>456-A, Green Street, Matara</td>
+                    <td>2024-11-20</td>
+                    <td>In Transit</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
 
 
 
