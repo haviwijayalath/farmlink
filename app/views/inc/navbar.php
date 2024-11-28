d5e25994cf01c4f839e7fdd5210f4d8935215a41
pawaniKumari <140165230+pawaniKumari@users.noreply.github.com>
2024-11-28 16:10:02 +0530

    add logout button only for buyer

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -9,8 +9,7 @@
 
     <?php if(isset($_SESSION['user_id']) || isset($_SESSION['admin_id'])) : ?>
 
-        <!-- <a href="<?php echo URLROOT; ?>/Users/logout" class="login-btn"><i class="fa-solid fa-user"></i> Logout</a> -->
-        <i class="fa-solid fa-bell"></i>
+        <a href="<?php echo URLROOT; ?>/Users/logout" class="login-btn"><i class="fa-solid fa-user"></i> Logout</a>
 
     <?php else : ?>
 
