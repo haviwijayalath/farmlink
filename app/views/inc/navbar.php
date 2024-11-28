fd606f487c71519db9db13b4287e724c23ad5c5d
pawaniKumari <140165230+pawaniKumari@users.noreply.github.com>
2024-11-28 16:16:44 +0530

    add cart icon

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -8,7 +8,8 @@
     <div style="display: flex; gap: 30px; font-size: 20px; font-family: 'Poppins', sans-serif;">
 
     <?php if(isset($_SESSION['user_id']) || isset($_SESSION['admin_id'])) : ?>
-
+        
+        <a href="<?php echo URLROOT; ?>/buyercontrollers/cartDetails"><i class="fa-solid fa-cart-shopping"></i></a>
         <a href="<?php echo URLROOT; ?>/Users/logout" class="login-btn"><i class="fa-solid fa-user"></i> Logout</a>
 
     <?php else : ?>
